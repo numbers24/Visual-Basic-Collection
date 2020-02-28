c2297aaf20715f20dc2b4fb8d3de0892d2982265
numbers24 <45888417+numbers24@users.noreply.github.com>
2020-02-28 15:40:56 -0500

    Add files via upload

---
+++ Main Form.vb
@@ -0,0 +1,25 @@
+﻿Public Class MainForm
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub everythingPictureBox_Click(sender As Object, e As EventArgs) Handles everythingPictureBox.Click
+        My.Computer.Audio.Play("Everything-Begins-by-danosongs.com.wav")
+    End Sub
+
+    Private Sub invisiblePictureBox_Click(sender As Object, e As EventArgs) Handles invisiblePictureBox.Click
+        My.Computer.Audio.Play("Invisible-Love-by-danosongs.com.wav")
+    End Sub
+
+    Private Sub magicPictureBox_Click(sender As Object, e As EventArgs) Handles magicPictureBox.Click
+        My.Computer.Audio.Play("Magic-Ghost-by-danosongs.com.wav")
+    End Sub
+
+    Private Sub threePictureBox_Click(sender As Object, e As EventArgs) Handles threePictureBox.Click
+        My.Computer.Audio.Play("Three-Drops-by-danosongs.com.wav")
+    End Sub
+
+    Private Sub unclaimedPictureBox_Click(sender As Object, e As EventArgs) Handles unclaimedPictureBox.Click
+        My.Computer.Audio.Play("Unclaimed-Territory-by-danosongs.com.wav")
+    End Sub
+End Class
